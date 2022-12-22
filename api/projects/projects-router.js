00af7670399c6ac11cdc2914ab999cc4c453d6b4
Brian Nguyen <brian.edison.nguyen@gmail.com>
2022-12-22 12:12:03 -0800

    feat: GET projects

--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -1,1 +1,19 @@
 // Write your "projects" router here!
+const express = require('express');
+const router = express.Router();
+const Projects = require('./projects-model');
+
+router.get('/', (req, res) => {
+  Projects.get()
+    .then((projects) => {
+      res.status(200).json(projects);
+    })
+    .catch((err) => {
+      res.status(500).json({
+        message: 'error when retrieving projects',
+        stack: err.stack,
+      });
+    });
+});
+
+module.exports = router;
